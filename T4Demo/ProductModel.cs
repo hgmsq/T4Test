eefd9082428222c9def33178f9085dfcf229f74a
github <431557313@qq.com>
2017-07-08 09:22:14 +0800

    + 加入解决方案

---
+++ T4Demo/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4Demo
+{
+   public class ProductModel
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string price { get; set; }
+    }
+}
---
+++ T4Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T4Demo;
+namespace T4Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductCategorys model = new ProductCategorys();
+            
+        }
+    }
+}
